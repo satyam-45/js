a51d544c05e5ba6e98abfeeab7113ad5b0209d94
satyam maurya <160267879+satyam-45@users.noreply.github.com>
2024-03-15 20:06:13 +0000

    variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId =123445
+let accountEmail = "satyam@google.com"
+var accountpassord = 12345
+accountCity = "azamgarh"
+let accountState;
+//    accountId = 2 //not allowed
+
+accountEmail ="sa@history.com"
+accountpassord ="01234"
+accountCity ="delhi"
+
+console.table([accountId,accountEmail,accountpassord,accountCity,accountState])
+/*
+prefer not to use var
+becouse of issues in block scope and functional scope
+
+*/
+
+
+
+console.log(accountId);
