c5ccf0b4faddcee1930443f3a7c0718a40cfbb79
satyam maurya <160267879+satyam-45@users.noreply.github.com>
2024-03-16 18:58:22 +0000

    conversion of datatypes

---
+++ 01_Basics/03-conversionOperation.js
@@ -0,0 +1,29 @@
+
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let velueInNumber =Number(score)
+//console.log(typeof velueInNumber);
+//console.log( velueInNumber);
+
+//"33"=>33
+// "33abc "=>NaN
+//true =>1; false=>0
+
+let isLoggedIn ="satyam"
+
+let booleanIsLoggedIn =(Boolean(isLoggedIn))
+//console.log(booleanIsLoggedIn);
+
+
+//1=>true ,0=>false
+//"" => false
+//"satyam"=> true
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
