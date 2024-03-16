38a590f22d4d9e16d3d60e56937ce51aa5813c24
satyam maurya <160267879+satyam-45@users.noreply.github.com>
2024-03-16 18:27:51 +0000

    datatypes

---
+++ 01_Basics/01-datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)//we are using  nodejs,not browser
+
+console.log(3+3);
+//code redebility should be high
+
+console.log("satyam");
+
+
+let name = "satyam"
+let age= 18
+let isLoggedIn = false
+let state;
+
+//number =>2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null =standlone value 
+//undefined => 
+//symbol =>unique
+
+
+//object
+
+console.log(typeof"satyam");
+console.log(typeof age);
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
